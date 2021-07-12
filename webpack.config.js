f6e2e366504f471efa3e465d6033da3bbd3d53f5
pashakiz <pashakiz@gmail.com>
2021-07-12 09:57:31 +0300

    refactor!: Update to webpack 5 (https://webpack.js.org/migrate/5/)

--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,6 @@ const cssLoaders = extra => {
   const loaders = [
     {
       loader: MiniCssExtractPlugin.loader,
-      // options: {
-      //   hmr: isDev,
-      //   reloadAll: true,
-      // },
     },
     {
       loader: 'css-loader',
